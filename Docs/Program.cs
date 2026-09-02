d696af826eacfccd721f678e6cb128d1a99239c2
jsakamoto <junichi.jp@gmail.com>
2026-09-03 08:34:43 +0900

    Prerender the documentation site and publish it to GitHub Pages

--- Docs/Program.cs
+++ Docs/Program.cs
@@ -7,12 +7,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<DocsCatalogService>();
-builder.Services.AddScoped<MarkdownService>();
-builder.Services.AddScoped<DocsUiState>();
-builder.Services.AddSingleton<EmbeddingService>();
-builder.Services.AddScoped<SearchIndexService>();
-builder.Services.AddScoped<ThemeService>();
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    services.AddScoped<DocsCatalogService>();
+    services.AddScoped<MarkdownService>();
+    services.AddScoped<DocsUiState>();
+    services.AddSingleton<EmbeddingService>();
+    services.AddScoped<SearchIndexService>();
+    services.AddScoped<ThemeService>();
+}
